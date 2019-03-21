f2d3ebf0a260928fa7028a96eec3a568fdeec04b
MicahJank <MicahJank@users.noreply.github.com>
2019-03-21 18:52:28 -0500

    roll-a-ball
    added ability to pick up collectables and dsiplay a score. As well as a you win message after collecting all of them.

--- roll-a-ball/Assets/Scripts/PlayerController.cs
+++ roll-a-ball/Assets/Scripts/PlayerController.cs
@@ -1,16 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody rb;
     [SerializeField] private float speed;
+    private int count;
+
+    public Text countText;
+    public Text winText;
 
 
     private void Start()
     {
+        count = 0;
         rb = GetComponent<Rigidbody>();
+        setCount();
+        winText.text = "";
     }
 
     private void FixedUpdate()
@@ -21,5 +29,26 @@ public class PlayerController : MonoBehaviour
         Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
 
         rb.AddForce(movement * speed);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pickup"))
+        {
+            count++;
+            setCount();
+            Destroy(other.gameObject);
+        }
+        
+    }
+
+
+    private void setCount()
+    {
+        countText.text = "Count: " + count.ToString();
+        if (count >= 12)
+        {
+            winText.text = "You win!";
+        }
     }
 }
